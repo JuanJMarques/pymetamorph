28362cfcd81549449afe1de54db3e6810f6fa2e5
JuanJMarques <juanjemdc@gmail.com>
2016-07-17 22:44:15 +0200

    Added label table to class fields generate label table function modified to only store jumps to final instructions print_disass funciton added

--- es/uc3m/pymetamorph/pymetamorph.py
+++ es/uc3m/pymetamorph/pymetamorph.py
@@ -1,10 +1,12 @@
 from __future__ import print_function
-from capstone import *
-from keystone import *
-import pefile
+
 import random
 import sys
+
+import pefile
+from capstone import *
 from capstone import x86_const
+from keystone import *
 
 
 class Pymetamorph(object):
@@ -16,9 +18,11 @@ class Pymetamorph(object):
         self.cs = Cs(CS_ARCH_X86, CS_MODE_32)
         self.cs.detail = True
         self.ks = Ks(KS_ARCH_X86, KS_MODE_32)
+        self.label_table = None
         if load_file:
             self.pe = pefile.PE(self.file)
             if self.debug:
+                print(self.pe.dump_info())
                 print('loading file')
             self.base_of_code = self.pe.OPTIONAL_HEADER.BaseOfCode
             code_section = self.find_section(self.base_of_code)
@@ -81,6 +85,8 @@ class Pymetamorph(object):
             initial_block_inst[i] = blocks[i][1][0]
             i += 1
             offset += size_of_blocks
+        if self.debug:
+            print('number of blocks: %d' % (len(blocks)))
         random.shuffle(blocks)
         instructions = []
         for block_number, block in blocks:
@@ -93,20 +99,24 @@ class Pymetamorph(object):
                 self.append_instruction(block, jump_inst, 0)
             instructions += block
         self.update_addresses(instructions, initial_address)
+        self.instructions = instructions
 
     def sort_instructions(self):
         sorted(self.instructions, key=lambda instruction: instruction.new_addr)
 
-    def generate_label_tables(self):
-        jmp_table = []
+    def generate_label_table(self):
+        jmp_table = dict()
         for inst in self.instructions:
-            print(
-                "0x%x:\t%s\t%s" % (inst.original_inst.address, inst.original_inst.mnemonic, inst.original_inst.op_str))
-            print('{},{},{},{}'.format(inst.original_inst.id, inst.original_inst.groups,
-                                       inst.original_inst.regs_read, inst.original_inst.regs_write))
-            if (x86_const.X86_INS_JAE <= inst.original_inst.id <= x86_const.X86_INS_JS) \
-                    or x86_const.X86_INS_CALL == inst.original_inst.id:
-                jmp_table.append(int(inst.original_inst.op_str, 16))
+            try:
+                if (x86_const.X86_INS_JAE <= inst.original_inst.id <= x86_const.X86_INS_JS) \
+                        or x86_const.X86_INS_CALL == inst.original_inst.id:
+                    addr = int(inst.original_inst.op_str, 16)
+                    jmp_table[addr] = addr
+                    # jmp_table['0x%x' % addr] = addr
+            except:
+                pass
+        # if self.debug:
+        print(sorted(jmp_table))
         return jmp_table
 
     @staticmethod
@@ -115,6 +125,59 @@ class Pymetamorph(object):
         for instruction in instructions:
             instruction.new_addr = address
             address += instruction.size
+
+    def print_disass(self):
+        out = open("code.asm", 'w')
+        for inst in self.instructions:
+            out.write("0x%x:\t%s\t%s\n" % (
+                inst.original_inst.address, inst.original_inst.mnemonic, inst.original_inst.op_str))
+            print(
+                "0x%x:\t%s\t%s" % (inst.original_inst.address, inst.original_inst.mnemonic, inst.original_inst.op_str))
+        out.close()
+
+    def write_file(self, path):
+        new_entry_point = self.locate_by_original_address(
+            self.pe.NT_HEADERS.OPTIONAL_HEADER.AddressOfEntryPoint).new_addr
+        self.pe.NT_HEADERS.OPTIONAL_HEADER.AddressOfEntryPoint = new_entry_point
+        if self.debug:
+            print(self.pe.dump_info())
+
+    def locate_by_original_address(self, Address):
+        for intstruction in self.instructions:
+            if intstruction.original_addr == Address:
+                return intstruction
+        return None
+
+    def update_label_table(self):
+        self.label_table = self.generate_label_table()
+        self.get_new_addresses_of_labels()
+
+    def get_new_addresses_of_labels(self):
+        for instruction in self.instructions:
+            if instruction.original_addr in self.label_table:
+                self.label_table[instruction.original_addr] = instruction.new_addr
+
+    def aply_label_table(self):
+        new_instructions = []
+        for instruction in self.instructions:
+            if (x86_const.X86_INS_JAE <= instruction.original_inst.id <= x86_const.X86_INS_JS) \
+                    or x86_const.X86_INS_CALL == instruction.original_inst.id:
+                new_jump = None
+                asm = None
+                for asm in self.ks.asm('%s 0x%x' % (
+                        instruction.original_inst.mnemonic,
+                        self.label_table[int(instruction.original_inst.op_str), 16])):
+                    break
+                for ins in self.cs.disasm(asm, 0):
+                    new_jump = MetaIns(ins)
+                    new_jump.original_addr = instruction.original_addr
+                    new_jump.new_addr = instruction.new_addr
+                    break
+                new_instructions.append(new_jump)
+            else:
+                new_instructions.append(instruction)
+        self.instructions = new_instructions
+
 
 
 class MetaIns(object):
@@ -130,7 +193,18 @@ class MetaIns(object):
 def main(file_path):
     meta = Pymetamorph(file_path, load_file=True)
     meta.shuffle_blocks()
+    meta.update_label_table()
+    meta.debug = True
+    meta.aply_label_table()
+    meta.write_file('meta.exe')
+    # meta.print_disass()
+
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         main(sys.argv[1])
+
+'''print(
+                "0x%x:\t%s\t%s" % (inst.original_inst.address, inst.original_inst.mnemonic, inst.original_inst.op_str))
+            print('{},{},{},{}'.format(inst.original_inst.id, inst.original_inst.groups,
+                                       inst.original_inst.regs_read, inst.original_inst.regs_write))'''
